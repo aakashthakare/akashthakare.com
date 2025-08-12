c2ed3986e264d9ab8796b2fe92d44709387cda0e
Akash <akashthakare@protonmail.com>
2025-08-12 15:49:05 +0530

    Initial Setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,18 @@
 const { DateTime } = require("luxon");
-const MarkdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
-
+  
   eleventyConfig.addFilter("date", (dateObj, format = "yyyy-MM-dd") => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(format);
+     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(format);
   });
 
   eleventyConfig.addCollection("posts", function(collection) {
-    return collection.getFilteredByGlob("./_posts/*.md").sort((a, b) => b.date - a.date);
+    return collection.getFilteredByGlob("./writing/*.md").sort((a, b) => b.date - a.date);
   });
 
-  const md = new MarkdownIt({ html: true, linkify: true });
-  eleventyConfig.addPairedShortcode("markdown", (content) => {
-    return md.render(content);
+  eleventyConfig.addFilter("currentYear", () => {
+    return DateTime.now().toFormat("yyyy");
   });
 
   return {
